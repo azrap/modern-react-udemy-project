c24d0a4ba280b99ba99df2a603f229e7744e22f8
azrap <azraap@gmail.com>
2020-01-11 14:41:53 -0800

    can display lat or error

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,12 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    // this is the only time we do direct assignment to this.state. All other times will be with setState
+    this.state = { lat: null, errorMessage: " " }; //we don't know what it is right now so we put null
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
+      position => {
+        //to update the state object we called setState once we get the position
+        this.setState({ lat: position.coords.latitude });
+      },
+      //when you call setState, you are only adding or changing, never removing or deleting. It leaves the rest of the things alone and only updates/adds what you tell it
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
-    return <div>Lattitude</div>;
+  }
+  // render() {
+  //   return (
+  //     <div>
+  //       Lattitude: {this.state.lat}
+  //       <br />
+  //       Error: {this.state.errorMessage}
+  //     </div>
+  //   );
+  // }
+
+  render() {
+    return (
+      <div>
+        Lattitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
