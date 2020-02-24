3b4cff5a4f8f26e381e21cda3e259a4592b99665
azrap <azraap@gmail.com>
2020-02-23 22:59:56 -0800

    learned about this binding

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,19 +2,32 @@ import React from "react";
 
 class SearchBar extends React.Component {
   // method gets called when someone changes the text in the bar
-  onInputChange(event) {
-    // changes everytime we type in the input
-    console.log(event.target.value);
-  }
+  // onInputChange(event) {
+  //   // changes everytime we type in the input
+  //   console.log(event.target.value);
+  // }
+  state = { term: "Search Term Here!" };
+
+  // if you use arrow function, it automatically makes sure this.state automatically binds to the component (Search Bar)
+  onFormSubmit = event => {
+    // keeps browser from automatically refreshing page on for submit
+    event.preventDefault();
+    console.log(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
 
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              // update state. term will get set to whatever you type in
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
