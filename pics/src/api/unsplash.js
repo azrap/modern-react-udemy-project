25b7fcfa4d33f9417fcd3d6191c332a839f83b8f
azrap <azraap@gmail.com>
2020-03-11 23:05:06 -0700

    key fix

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID zANRjgR9ZVozLT-kBfPchyjqMcjqQd7lmvq9Tw-OGWo"
+    Authorization: " key "
   }
 });
