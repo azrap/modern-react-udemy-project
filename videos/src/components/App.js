2680a70e9e9d6eb877dac0d06a8995cb11841f62
azrap <azraap@gmail.com>
2020-03-19 16:10:54 -0700

    YouTube key

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,7 +4,8 @@ import SearchBar from "./SearchBar";
 class App extends React.Component {
   render() {
     return (
-      <div>
+      // the className below gives a margin to the search bar
+      <div className="ui container">
         <SearchBar />
       </div>
     );
--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,8 +1,34 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  // setting state to default, which is controlled by the input below
+  state = { term: "" };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+  };
+
+  //   todo: call calback from parent component
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
