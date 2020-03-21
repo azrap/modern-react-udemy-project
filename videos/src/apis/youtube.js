becefd264a64b942ac47ebf5528e6f90119247a7
azrap <azraap@gmail.com>
2020-03-20 19:58:48 -0700

    almost done with the video chapter

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,16 @@
+import axios from "axios";
+require("dotenv").config();
 
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    type: "video",
+    key: process.env.REACT_APP_YT_KEY
+  }
+});
+
+// const YT_KEY = ;
+
+// console.log(YT_KEY);
--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,12 +1,37 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+
+require("dotenv").config();
 
 class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  //   once the search term has been submitted do this:
+  onTermSubmit = async term => {
+    console.log(term);
+    const response = await youtube.get("/search", {
+      params: {
+        q: term
+      }
+    });
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = video => {
+    console.log("fro the app", video);
+  };
+
   render() {
     return (
       // the className below gives a margin to the search bar
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
