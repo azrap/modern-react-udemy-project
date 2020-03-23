2fde25c5dce88143c6d6c27425e7f5df46b4cdba
azrap <azraap@gmail.com>
2020-03-23 13:45:22 -0700

    redux code and some styling

---
+++ songs/src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { selectSong } from "../actions";
+import Songlist from "./SongList";
+
+const App = () => {
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <Songlist />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
