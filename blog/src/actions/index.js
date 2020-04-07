1a461a735140c2ebfc585f4e32386a6f0d46f128
azrap <azraap@gmail.com>
2020-04-07 13:07:09 -0700

    adding dispatch and getState

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,8 @@
 import jsonPlaceholder from "../api/jsonPlaceholder";
 
 export const fetchPosts = () => {
-  return function () {
+  return function (dispatch, getState) {
+    const promise = jsonPlaceholder.get("/post");
     return {
       type: "FETCH_POSTS",
       payload: " ",
