cb0993e665f1e49efcfaf3816ce4196a67ebdf40
azrap <azraap@gmail.com>
2020-04-25 14:51:17 -0700

    google auth hooked up

--- streams/src/components/GoogleAuth.js
+++ streams/src/components/GoogleAuth.js
@@ -1,8 +1,35 @@
 import React from "react";
+require("dotenv").config();
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId: process.env.REACT_APP_GOOGLE_STREAMS_CLIENT_ID,
+          scope: "email",
+        })
+        .then(() => {
+          // get the auth object from the google api
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
+    });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know of if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
