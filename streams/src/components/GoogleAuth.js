0b3c31981124e10d750a55a30d7ade07f264946a
azrap <azraap@gmail.com>
2020-04-27 12:31:52 -0700

    comments to understand what's hapening better

--- streams/src/components/GoogleAuth.js
+++ streams/src/components/GoogleAuth.js
@@ -15,7 +15,7 @@ class GoogleAuth extends React.Component {
           this.auth = window.gapi.auth2.getAuthInstance();
 
           //below we set/display the initial state of the user after initializing the authentication library:
-          this.onAuthChange();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
 
           //.listen gets invoked anytime the authentication status changes from sign in to out etc. and executes whatever you put inside it.
           this.auth.isSignedIn.listen(this.onAuthChange);
