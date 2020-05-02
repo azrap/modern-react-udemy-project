08ded89374be48a8fed741af9430dea7c558f604
azrap <azraap@gmail.com>
2020-05-02 13:12:17 -0700

    input forms and labels

--- streams/src/components/streams/StreamCreate.js
+++ streams/src/components/streams/StreamCreate.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div> StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  // the Field below from redux-form will want to pass in a bunch of arguments into renderInput. We can put them under one obj and call it formProps
+  renderInput(formProps) {
+    const { input, label } = formProps;
+    return (
+      <div className="field">
+        <label> {label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
 
-export default StreamCreate;
+  render() {
+    console.log(this.props);
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+// reduxForm is a higher order function i.e. it returns a function, which then calls StreamCreate
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
