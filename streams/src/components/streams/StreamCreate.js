b9662be7e1b48412b8c25bee6a317dc8b99c5e1f
azrap <azraap@gmail.com>
2020-05-03 16:54:29 -0700

    stream create

--- streams/src/components/streams/StreamCreate.js
+++ streams/src/components/streams/StreamCreate.js
@@ -1,30 +1,71 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
+import { Field, reduxForm, formValues } from "redux-form";
 
 class StreamCreate extends React.Component {
   // the Field below from redux-form will want to pass in a bunch of arguments into renderInput. We can put them under one obj and call it formProps
-  renderInput(formProps) {
-    const { input, label } = formProps;
+
+  renderError = (formProps) => {
+    const { touched, error } = formProps;
+
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+  renderInput = (formProps) => {
+    const { input, label, meta } = formProps;
+    const className = `field ${meta.error && meta.touched ? "error" : " "}`;
+
     return (
-      <div className="field">
+      <div className={className}>
         <label> {label}</label>
         <input {...input} />
+        <div>{this.renderError(meta)}</div>
       </div>
     );
+  };
+
+  onSubmit(formProps) {
+    console.log(formProps);
   }
 
   render() {
     console.log(this.props);
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        autoComplete="off"
+        className="ui form error"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
 }
 
+// the below helps us validate the form inputs submitted by a user
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+    //only ran if the user did not enter a title
+  }
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+  return errors;
+};
 // reduxForm is a higher order function i.e. it returns a function, which then calls StreamCreate
 export default reduxForm({
   form: "streamCreate",
+  validate,
 })(StreamCreate);
