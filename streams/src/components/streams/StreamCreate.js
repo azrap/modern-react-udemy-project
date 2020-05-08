0b58d55cbf6ee1d62d89181618c3af4c738e3e54
azrap <azraap@gmail.com>
2020-05-08 12:30:48 -0700

    edit/delete buttons + create button

--- streams/src/components/streams/StreamCreate.js
+++ streams/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Field, reduxForm, formValues } from "redux-form";
+import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   // the Field below from redux-form will want to pass in a bunch of arguments into renderInput. We can put them under one obj and call it formProps
@@ -28,13 +30,15 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formProps) {
-    console.log(formProps);
-  }
+  // the below triggers the action creator 'createStream'
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     console.log(this.props);
     return (
+      // handlesubmit is a redux-form function I htink?
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
         autoComplete="off"
@@ -65,7 +69,9 @@ const validate = (formValues) => {
   return errors;
 };
 // reduxForm is a higher order function i.e. it returns a function, which then calls StreamCreate
-export default reduxForm({
+const formWrapper = reduxForm({
   form: "streamCreate",
   validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapper);
--- streams/src/reducers/index.js
+++ streams/src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as reduxFormReducer } from "redux-form";
-
+import streamReducer from "./streamsReducer";
 import authReducer from "./authReducer";
 
 export default combineReducers({
   auth: authReducer,
   form: reduxFormReducer,
+  streams: streamReducer,
 });
