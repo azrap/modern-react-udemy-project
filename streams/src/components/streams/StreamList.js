67dc70d3a35a5bb9d6c9aee88b7cf4d8cb6f6e26
azrap <azraap@gmail.com>
2020-05-10 15:01:34 -0700

    linking edit and delete components

--- streams/src/components/streams/StreamList.js
+++ streams/src/components/streams/StreamList.js
@@ -12,13 +12,20 @@ class StreamList extends React.Component {
     if (stream.userId == this.props.currentUserId) {
       return (
         <div className="right float content">
-          <button className="ui button primary"> Edit </button>
-          <button className="ui button negative"> Delete </button>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
+          <Link
+            to={`/streams/delete/${stream.id}`}
+            className="ui button negative"
+          >
+            Delete
+          </Link>
         </div>
       );
     }
   }
-  
+
   renderList() {
     return this.props.streams.map((stream) => {
       return (
