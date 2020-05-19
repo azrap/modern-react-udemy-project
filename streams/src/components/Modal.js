1e12da0ff8da28af62dbec26a53941e7e80274bc
azrap <azraap@gmail.com>
2020-05-18 22:05:18 -0700

    delete modal styled

--- streams/src/components/Modal.js
+++ streams/src/components/Modal.js
@@ -4,7 +4,14 @@ import ReactDOM from "react-dom";
 const Modal = (props) => {
   return ReactDOM.createPortal(
     <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">Gibberish</div>
+      <div className="ui standard modal visible active">
+        <div className="header">Delete Stream</div>
+        <div className="content">Are you sure you want to delete stream?</div>
+        <div className="actions">
+          <button className="ui button">Delete</button>
+          <button className="ui primary button">Cancel</button>
+        </div>
+      </div>
       {/* // you need the below + to hook it up to index.html // also need to add{" "} */}
       {/* <div id="modal"></div> under the root in index.html */}
     </div>,
--- streams/src/components/streams/StreamDelete.js
+++ streams/src/components/streams/StreamDelete.js
@@ -3,7 +3,11 @@ import Modal from "../Modal";
 
 const StreamDelete = () => {
   return (
-    <div>
+    <div className="ui dimmer modals visible active">
+      <div className="ui standard modal visible active">
+        <div className="header">Dete Stream</div>
+        <div className="content">Are you sure you want to delete stream?</div>
+      </div>
       StreamDelete
       <Modal />
     </div>
