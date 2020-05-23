7636eb563840057cb85f0b4eae726fb1ae11ed64
azrap <azraap@gmail.com>
2020-05-22 23:13:29 -0700

    improve the modal

--- streams/src/components/Modal.js
+++ streams/src/components/Modal.js
@@ -1,24 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div
-      onClick={() => history.push("/")}
-      className="ui dimmer modals visible active"
-    >
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
       {/* e.stopPropogation prevents the outer div (i.e the page w the on click history.push('/') that dismisses modal) from propogating to inner div (i.e dismiss on clicking the Modal) */}
       <div
         onClick={(e) => e.stopPropagation()}
         className="ui standard modal visible active"
       >
-        <div className="header">Delete Stream</div>
-        <div className="content">Are you sure you want to delete stream?</div>
-        <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
-        </div>
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
       {/* // you need the below + to hook it up to index.html // also need to add{" "} */}
       {/* <div id="modal"></div> under the root in index.html */}
--- streams/src/components/streams/StreamDelete.js
+++ streams/src/components/streams/StreamDelete.js
@@ -1,15 +1,28 @@
 import React from "react";
 import Modal from "../Modal";
+import history from "../../history";
 
 const StreamDelete = () => {
+  const onDismiss = () => history.push("/");
+
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative"> Delete</button>
+      <button onClick={onDismiss} className="ui button">
+        Cancel
+      </button>
+    </React.Fragment>
+  );
+
   return (
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
-        <div className="header">Dete Stream</div>
-        <div className="content">Are you sure you want to delete stream?</div>
-      </div>
-      StreamDelete
-      <Modal />
+    <div>
+      Delete Stream
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={onDismiss}
+      />
     </div>
   );
 };
