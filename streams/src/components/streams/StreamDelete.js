5f2e39c49dcf263f05fab12a185586cf772330a0
azrap <azraap@gmail.com>
2020-05-22 23:42:02 -0700

    commit modal take two

--- streams/src/components/streams/StreamDelete.js
+++ streams/src/components/streams/StreamDelete.js
@@ -2,29 +2,33 @@ import React from "react";
 import Modal from "../Modal";
 import history from "../../history";
 
-const StreamDelete = () => {
-  const onDismiss = () => history.push("/");
+class StreamDelete extends React.Component {
+  componentDidMount() {}
 
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative"> Delete</button>
-      <button onClick={onDismiss} className="ui button">
-        Cancel
-      </button>
-    </React.Fragment>
-  );
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className="ui button negative"> Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  }
 
-  return (
-    <div>
-      Delete Stream
-      <Modal
-        title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
-        onDismiss={onDismiss}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        Delete Stream
+        <Modal
+          title="Delete Stream"
+          content="Are you sure you want to delete this stream?"
+          actions={this.renderActions()}
+          onDismiss={() => {
+            history.push("/");
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 export default StreamDelete;
