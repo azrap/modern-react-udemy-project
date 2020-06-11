cdeec467af0af457252187c27101747fcaffd75c
azrap <azraap@gmail.com>
2020-06-10 19:46:48 -0700

    added a basic color palette

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import ColorBox from "./ColorBox";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Palette palette={seedColors[4]} />
+        {/* the below passes all key/object pairs inside seedColors[4] in as props eg colors, emoji, id etc */}
+        <Palette {...seedColors[4]} />
       </div>
     );
   }
