1d4966bc88b756818da4b059c77ee8de4ad3b57c
azrap <azraap@gmail.com>
2020-06-20 19:24:22 -0700

    state sample exercise committed

--- introducing-state/src/App.js
+++ introducing-state/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import Rando from "./Rando";
 import "./App.css";
+import Button from "./Button";
+import BrokenClick from "./BrokenClick";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Rando maxNum={7} />
+        <Button />
+        <BrokenClick />
       </div>
     );
   }
