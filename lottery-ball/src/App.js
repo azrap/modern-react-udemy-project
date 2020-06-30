449bb6a87bfdcd6b6073368544dc92932622c22d
azrap <azraap@gmail.com>
2020-06-29 23:48:12 -0700

    finished project

---
+++ lottery-ball/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Ball from "./Ball";
+import Lottery from "./Lottery";
+
+function App() {
+  return (
+    <div className="App">
+      <Lottery />
+    </div>
+  );
+}
+
+export default App;
